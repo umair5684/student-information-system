15e595670ec0fc5cfc366438a7cca5698df7f961
umair5684 <77715483+umair5684@users.noreply.github.com>
2022-04-06 16:31:14 +0500

    Update form.blade.php

--- resources/views/student/form.blade.php
+++ resources/views/student/form.blade.php
@@ -1,26 +1,26 @@
 <div class="  mt-5 d-flex justify-content-center">
     <div class=" col-3">
         <label for="fullname" class="form-label">Full Name</label>
-        <input type="text" class="form-control" name="fullname" value="{{ old('fullname') }}">
+        <input type="text" class="form-control" name="fullname" value="{{ $student->fullname }}">
     </div>
 
 </div>
 <div class=" d-flex justify-content-center">
     <div class="col-3">
         <label for="rollno" class="form-label">Roll No</label>
-        <input type="text" class="form-control" name="rollno" value="{{ old('rollno') }}">
+        <input type="text" class="form-control" name="rollno" value="{{ $student->rollno }}">
     </div>
 </div>
 <div class=" d-flex justify-content-center">
     <div class="col-3">
         <label for="program" class="form-label">Program</label>
-        <input type="text" class="form-control" name="program" value="{{ old('program') }}">
+        <input type="text" class="form-control" name="program" value="{{ $student->program}}">
     </div>
 </div>
 <div class=" d-flex justify-content-center">
     <div class="col-3">
         <label for="semester" class="form-label">Semester</label>
-        <input type="text" class="form-control" name="semester" value="{{ old('semester') }}">
+        <input type="text" class="form-control" name="semester" value="{{ $student->semester }}">
     </div>
 </div>
 
@@ -29,7 +29,7 @@
 <div class=" d-flex justify-content-center">
     <div class="col-3">
         <label for="phone_no" class="form-label">Phone No</label>
-        <input type="number" class="form-control" name="phone_no" value="{{ old('phone_no') }}">
+        <input type="number" class="form-control" name="phone_no" value="{{$student->phone_no }}">
     </div>
 </div>
 
@@ -37,12 +37,12 @@
 <div class=" d-flex justify-content-center">
     <div class="col-3">
         <label for="address" class="form-label">Address</label>
-        <input type="text" class="form-control" name="address" value="{{ old('address') }}">
+        <input type="text" class="form-control" name="address" value="{{ $student->address') }}">
     </div>
     <div class=" d-flex justify-content-center">
         <div class="col-3">
             <label for="email" class="form-label">Email</label>
-            <input type="text" class="form-control" name="email" value="{{ old('email') }}">
+            <input type="text" class="form-control" name="email" value="{{$student->email }}">
         </div>
 </div>
 <div class=" d-flex justify-content-center">
